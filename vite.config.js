0b634f5b5dc6d9694ab338c5a5376e89e60d2b27
Jacob Welsh <jacobwelsh42@gmail.com>
2025-03-18 18:46:55 +0100

    Update deployment configurstion

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// filepath: c:\Users\jacob\Desktop\Software Developer\e-plantShopping\vite.config.js
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  base: "/shoppingreact",
   plugins: [react()],
-})
+  base: '/e-plantShopping/', // Add your repository name here
+});
